00f2882d54334c5301fc6882b8956bfac458d2d8
BK Sarthak Das <bksarthak@users.noreply.github.com>
2015-01-21 00:49:53 -0800

    Update shake.py

--- shake.py
+++ shake.py
@@ -1,4 +1,27 @@
 import random
 
 def generate(length):
- chars = 
+ char = string.ascii_lowercase+' '
+ randstr = ''
+ for i in range(length):
+  randstr=randstr+char[random.randrange(27)]
+  return randstr
+ 
+def score(final,test):
+ numScore = 0
+ for i in range(len(final)):
+  if final[i]==test[i]:
+   numScore = numScore + 1
+ return numScore/len(final)
+ 
+def main():
+ final = 'methinks it is like a weasel'
+ random = generate(23)
+ best = 0
+ newscore = score(final,random)
+ while newscore < 1:
+  if newscore > best:
+   print(newscore,random)
+   best = newscore
+  random = generate(23)
+  newscore = score(final,random)
